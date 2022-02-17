a558edd3d71ae07ab6a06160fe3f502eebe6c2d9
Johnny.vaca <johnny.vaca-jaramillo@cpnv.ch>
2022-02-17 16:48:42 +0100

    creation du menu wip

--- code/src/main/react-native/App.js
+++ code/src/main/react-native/App.js
@@ -1,64 +1,78 @@
-// In App.js in a new project
-/*
-import * as React from 'react';
-import Gabarit from './Gabarit';
-import {View} from "react-native";
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,Image,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+import { NavigationBar } from 'navigationbar-react-native';
 
-
-const App = () => {
-    return (
-        <View>
-            <Gabarit />
+const ComponentLeft = () => {
+    return(
+        <View style={{ flex: 1, alignItems: 'flex-start', backgroundColor:'yellow'}} >
+            <TouchableOpacity style={ {justifyContent:'center', flexDirection: 'row'}}>
+                <Image
+                    source={require('./img/ic_back.png')}
+                    style={{ resizeMode: 'contain', width: 20, height: 20, alignSelf: 'center' }}
+                />
+                <Text style={{ color: 'blue', }}>Back Home</Text>
+            </TouchableOpacity>
         </View>
     );
+};
+
+const ComponentCenter = () => {
+    return(
+        <View style={{ flex: 1, backgroundColor: 'blue'}}>
+            <Image
+                source={require('./img/ic_logo.png')}
+                style={{resizeMode: 'contain', width: 200, height: 35, alignSelf: 'center' }}
+            />
+        </View>
+    );
+};
+
+const ComponentRight = () => {
+    return(
+        <View style={{ flex: 1, alignItems: 'flex-end', backgroundColor:'red' }}>
+            <TouchableOpacity>
+                <Text style={{ color: 'white', }}> Rights </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.navbar}>
+                <NavigationBar
+                    componentLeft={ () => <ComponentLeft />}
+                    componentCenter   = { () =>  <ComponentCenter /> }
+                    componentRight    = { () =>  <ComponentRight />  }
+                    navigationBarStyle= {{ backgroundColor: 'orange' }}
+                    statusBarStyle    = {{ barStyle: 'light-content', backgroundColor: '#215e79' }}
+                />
+                </View>
+            </View>
+        );
+    }
 }
 
-export default App;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: 'rgba(219,220,208,0.92)',
 
-
-*/
-
-// In App.js in a new project
-
-import * as React from 'react';
-import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function DetailsScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen21</Text>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Overview111'}}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{title: 'Overview2'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
+    },
+    navbar : {
+        position: 'absolute',
+        top: '10%',
+        left: 0,
+        right: 0,
+    }
+});
 export default App;
